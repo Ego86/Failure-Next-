0533e8d58a67c4bce38f1cdac4e6e6ff462a3d80
Ego86 <endeerfed215@mail.com>
2025-05-05 14:45:33 +0500

    upd layout

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -1,12 +1,21 @@
+"use client"
+
+import useWindowSize from "@/shared/hook/useWindowSize";
 import { SideBar } from "@/shared/ui/sidebar";
+import TabBarMobile from "@/shared/ui/tabBarMobile/tabBarMobile";
 import Header from "@/widgets/header";
 import { ReactNode } from "react";
 
 const Layout = ({ children, isAuth }: {isAuth?: boolean, children: ReactNode}) => {
+  const [width] = useWindowSize()
   return (
     <>
-      {isAuth && <Header />}
-      {isAuth && <SideBar />}
+      {isAuth && 
+      <>
+      <Header />
+      {width > 1000 ? <SideBar /> : <TabBarMobile/>}
+      </>
+      }
       {children}
     </>
   );
