de966077eb6a9765c0233899c463c425da6c0bd6
Ego86 <endeerfed215@mail.com>
2025-05-06 15:56:33 +0500

    upd connect

--- src/shared/hook/useWebSocket.ts
+++ src/shared/hook/useWebSocket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 
 const useWebSocket = () => {
   const [messages, setMessages] = useState<string[]>([])
-  const socket = useRef(io("http://localhost:3002"));
+  const socket = useRef(io("https://failure-next-pjy861hrp-failures-projects.vercel.app"));
 
   useEffect(() => {
     const socketInstance = socket.current;
