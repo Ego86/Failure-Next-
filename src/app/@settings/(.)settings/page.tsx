381a869f6520b54f725eb3dee14df1a6f1e23e6a
Ego86 <endeerfed215@mail.com>
2025-10-17 12:53:13 +0500

    fix modal window settings

--- src/app/@settings/(.)settings/page.tsx
+++ src/app/@settings/(.)settings/page.tsx
@@ -1,32 +1,39 @@
-"use client"
-import ButtonChangeTheme from "@/features/button-change-theme";
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import Modal from "@/features/modal";
 import Button from "@/shared/ui/button/Button";
 import PlatfomSection from "@/shared/ui/platformSection/UI/platfomSection";
-import { useRouter } from "next/navigation";
+import ButtonChangeTheme from "@/features/button-change-theme";
 
-const Settings = () => {
+export default function SettingsModal() {
   const router = useRouter();
+  const pathname = usePathname();
+  const [visible, setVisible] = useState(true);
 
-  const handleBack = () => {
-    router.back(); 
-  };
+  // Закрываем при изменении пути
+  useEffect(() => {
+    if (!pathname.includes("/settings")) setVisible(false);
+  }, [pathname]);
 
-  const handleExit = () => {
-    router.replace("/auth/login", { scroll: false }); // правильный вызов
-  };
+  if (!visible) return null;
 
   return (
     <Modal>
       <PlatfomSection className="w-[500px] p-5 h-[500px] flex flex-col">
-        <Button className="mb-5" onClick={handleBack}>Back</Button>
-        <Button className="mb-4" variant="containd" onClick={handleExit}>
+        <Button className="mb-5" onClick={() => router.back()}>
+          Back
+        </Button>
+        <Button
+          className="mb-4"
+          variant="containd"
+          onClick={() => router.replace("/auth/login")}
+        >
           Exit
         </Button>
         <ButtonChangeTheme />
       </PlatfomSection>
     </Modal>
   );
-};
-
-export default Settings;
+}
